ca9332bb17cfdd4d80aa543b6352966d1273f68d
TitouanLC <84964261+Titouan44330@users.noreply.github.com>
2024-06-24 14:49:22 +0200

    fix App main

--- src/main/java/org/ss/orchestre/couplagefort/App.java
+++ src/main/java/org/ss/orchestre/couplagefort/App.java
@@ -1,22 +1,18 @@
 package org.ss.orchestre.couplagefort;
 public class App {
 	public static void main(String[] args) {
+		Morceau morceau = new Morceau("La 9eme de Beethoven");
+
 		System.out.println("Le pianiste : ");
-		Piano piano = new Piano();
-		Morceau morceau1 = new Morceau("La 9eme de Beethoven");
-		Musicien pianiste = new Musicien(piano,morceau1);
+		Musicien pianiste = new Musicien(new Piano(),morceau);
 		pianiste.jouerMorceau();
 
 		System.out.println("Le violoniste : ");
-		Violon violon = new Violon();
-		Morceau morceau2 = new Morceau("La 9eme de Beethoven");
-		Musicien violoniste = new Musicien(violon,morceau2);
+		Musicien violoniste = new Musicien(new Violon(),morceau);
 		violoniste.jouerMorceau();
 
 		System.out.println("Le batteur : ");
-		Batterie batterie = new Batterie();
-		Morceau morceau3 = new Morceau("La 9eme de Beethoven");
-		Musicien batteur = new Musicien(batterie, morceau3);
+		Musicien batteur = new Musicien(new Batterie(), morceau);
 		batteur.jouerMorceau();
 
 		System.out.println("\n L'orchestre : ");
